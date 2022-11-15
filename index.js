057fe1c71cd5d6599cb0f51e8da35d1cf4cd3d96
Elias Garcia <eliasgarciadev@gmail.com>
2022-11-15 15:47:04 +0100

    Add new rules and plugins

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
   },
-  settings: {
-    react: {
-      version: "17",
-    },
-  },
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:json/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:typescript-sort-keys/recommended",
   ],
-  ignorePatterns: ["/node_modules", "/build/", "/public/"],
+  ignorePatterns: ["build", "node_modules", "package-lock.json"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: "./tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "import",
+    "json",
+    "jsx-a11y",
+    "react",
+    "react-hooks",
+    "sort-destructure-keys",
+    "sort-keys-fix",
+    "typescript-sort-keys",
+  ],
+  project: "./tsconfig.json",
   rules: {
-    "react/prop-types": "off",
+    "import/order": ["error", { alphabetize: { order: "asc", caseInsensitive: true } }],
+    "no-console": ["warn", { allow: ["error"] }],
+    "react/jsx-curly-brace-presence": "warn",
     "react/display-name": "off",
-    "react/jsx-curly-brace-presence": "warn"
+    "react/jsx-sort-props": ["error", { ignoreCase: true }],
+    "react-hooks/exhaustive-deps": "error",
+    "sort-imports": ["error", { ignoreDeclarationSort: true }],
+    "sort-destructure-keys/sort-destructure-keys": ["error", { caseSensitive: false }],
+    "sort-keys-fix/sort-keys-fix": ["error", "asc", { caseSensitive: false }],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    react: {
+      version: "detect",
+    },
   },
 };
